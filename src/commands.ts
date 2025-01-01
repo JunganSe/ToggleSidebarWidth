cfc5bb45a407005fab53ccfdc6fddfca21dffddb
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-01-01 15:06:39 +0100

    Add commands

---
+++ src/commands.ts
@@ -0,0 +1,16 @@
+import * as vscode from 'vscode';
+
+export function toggleWidth(context: vscode.ExtensionContext) {
+    // TODO: Implement toggle.
+    console.log('toggleWidth activated');
+}
+
+export function restoreDefaultWidth(context: vscode.ExtensionContext) {
+    // TODO: Implement restoreDefaultWidth.
+    console.log('restoreDefaultWidth activated');
+}
+
+export function restoreStoredWidth(context: vscode.ExtensionContext) {
+    // TODO: Implement restoreStoredWidth.
+    console.log('restoreStoredWidth activated');
+}
