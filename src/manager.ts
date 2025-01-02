6da60b5ea71eacea6c68c1043578e50b05682889
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-01-02 21:02:54 +0100

    Fix self reference error in SidebarWidthManager

--- src/manager.ts
+++ src/manager.ts
@@ -3,40 +3,43 @@ import { StorageWorker } from './storageWorker';
 import { VscodeWorker } from './vscodeWorker';
 import { DEFAULT_DEFAULT_WIDTH, DEFAULT_STORED_WIDTH } from './constants';
 
+let self: SidebarWidthManager;
+
 export class SidebarWidthManager {
     private readonly storageWorker: StorageWorker;
     private readonly vscodeWorker: VscodeWorker;
 
     constructor(context: vscode.ExtensionContext) {
+        self = this;
         this.storageWorker = new StorageWorker(context);
         this.vscodeWorker = new VscodeWorker(context);
     }
 
     public toggleWidth() {
-        const currentWidth = this.vscodeWorker.getSidebarWidth();
-        const defaultWidth = this.storageWorker.getDefaultWidth() ?? DEFAULT_DEFAULT_WIDTH;
+        const currentWidth = self.vscodeWorker.getSidebarWidth();
+        const defaultWidth = self.storageWorker.getDefaultWidth() ?? DEFAULT_DEFAULT_WIDTH;
 
         if (currentWidth === defaultWidth) 
-            this.restoreStoredWidth();
+            self.restoreStoredWidth();
          else 
-            this.restoreDefaultWidth();
+            self.restoreDefaultWidth();
     }
 
     public restoreDefaultWidth() {
-        const currentWidth = this.vscodeWorker.getSidebarWidth();
-        const defaultWidth = this.storageWorker.getDefaultWidth() ?? DEFAULT_DEFAULT_WIDTH;
+        const currentWidth = self.vscodeWorker.getSidebarWidth();
+        const defaultWidth = self.storageWorker.getDefaultWidth() ?? DEFAULT_DEFAULT_WIDTH;
 
         if (currentWidth !== defaultWidth) {
-            this.storageWorker.setStoredWidth(currentWidth);
-            this.vscodeWorker.setSidebarWidth(defaultWidth);
+            self.storageWorker.setStoredWidth(currentWidth);
+            self.vscodeWorker.setSidebarWidth(defaultWidth);
         }
     }
 
     public restoreStoredWidth() {
-        const currentWidth = this.vscodeWorker.getSidebarWidth();
-        const storedWidth = this.storageWorker.getStoredWidth() ?? DEFAULT_STORED_WIDTH;;
+        const currentWidth = self.vscodeWorker.getSidebarWidth();
+        const storedWidth = self.storageWorker.getStoredWidth() ?? DEFAULT_STORED_WIDTH;;
 
         if (currentWidth !== storedWidth) 
-            this.vscodeWorker.setSidebarWidth(storedWidth);
+            self.vscodeWorker.setSidebarWidth(storedWidth);
     }
 }
